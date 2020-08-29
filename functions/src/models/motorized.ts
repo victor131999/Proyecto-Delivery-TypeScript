a35efb68488432a20082257595dd28301fe0f8dc
victor131999 <tovir.gu.hu@hotmail.com>
2020-08-28 21:55:49 -0500

    aumento de atributos a motorized

--- functions/src/models/motorized.ts
+++ functions/src/models/motorized.ts
@@ -2,14 +2,18 @@
 export interface Motorized {
     idmotorized?:string;
     brand : string;
-    vehicle : string
+    vehicle : string;
+    owner:string;
+    disponibility:string
 }
 export function Motorized( data:any,id?:string){
-    const { brand,vehicle } = data;
+    const { brand,vehicle,owner,disponibility } = data;
     let object:Motorized={
         idmotorized:id,
         brand:brand,
-        vehicle:vehicle
+        vehicle:vehicle,
+        owner:owner,
+        disponibility:disponibility
     };
     return object;
 }
