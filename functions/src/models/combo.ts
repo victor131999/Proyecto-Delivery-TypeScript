3be125816adcca2f350e4c392412734911c88eca
victor131999 <tovir.gu.hu@hotmail.com>
2021-03-27 01:14:25 -0500

    Aumento del campo descripcion en los combos

--- functions/src/models/combo.ts
+++ functions/src/models/combo.ts
@@ -4,15 +4,17 @@ export interface Combo {
     fusion : string;
     cost : string;
     image : string;
+    description: string;
 
 }
 export function Combo( data:any,id?:string){
-    const { fusion,cost,image } = data;
+    const { fusion,cost,image, description } = data;
     let object:Combo={
         idCombo:id,
         fusion:fusion,
         cost:cost,
         image:image,
+        description:description,
 
     };
     return object;
